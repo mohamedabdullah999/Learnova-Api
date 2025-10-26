08123b03b33e0f9133c0e57889c76fcbdc53f99b
mohamedabdullah999 <mohaammed2562005@gmail.com>
2025-10-26 10:22:47 +0300

    fix security for courses

--- routes/api/courses.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Courses\CourseController;
-
-Route::apiResource('/courses', CourseController::class)
-    ->only('index', 'show');
